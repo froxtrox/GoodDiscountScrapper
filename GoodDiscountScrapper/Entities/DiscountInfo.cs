bbbe90d86e0dc5cc851374c73e8428076ff964c2
gz l <16388413+froxtrox@users.noreply.github.com>
2022-01-18 22:26:01 +0000

    code clean up

--- GoodDiscountScrapper/Entities/DiscountInfo.cs
+++ GoodDiscountScrapper/Entities/DiscountInfo.cs
@@ -4,9 +4,9 @@
     {
         public string Name { get; set; }
         public string Link { get; set; }
-        public string Category { get; set; } = "Shoe";
+        public string Category { get; set; }
 
-        public string Brand { get; set; } = "Jordan";
+        public string Brand { get; set; }
 
         public string OriginalPrice { get; set; }
 
--- GoodDiscountScrapper/Startup.cs
+++ GoodDiscountScrapper/Startup.cs
@@ -2,16 +2,11 @@ using GoodDiscountScrapper.Interfaces;
 using GoodDiscountScrapper.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Mail;
-using System.Threading.Tasks;
 
 namespace GoodDiscountScrapper
 {
